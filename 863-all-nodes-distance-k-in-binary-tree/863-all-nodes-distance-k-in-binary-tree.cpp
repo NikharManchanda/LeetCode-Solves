b3455dafe492f516eab6e5e683d5d3d603501d05
Nikhar Manchanda <60578798+NikharManchanda@users.noreply.github.com>
2022-07-01 23:25:53 +0530

    Time: 12 ms (39.58%), Space: 15 MB (23.99%) - LeetHub

---
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    TreeNode* tar;
+    queue<TreeNode*> q;
+    int vis[505];
+    unordered_map<TreeNode*,TreeNode*> par;
+    void fun(TreeNode* root,TreeNode* p)
+    {
+        if(!root)
+            return;
+        if(root==tar)
+        {
+            q.push(root);
+            vis[root->val]=1;
+        }
+        if(p)
+            par[root]=p;
+        fun(root->right,root);
+        fun(root->left,root);
+    }
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        tar=target;
+        fun(root,nullptr);
+        vector<int> ans;
+        while(!q.empty())
+        {
+            auto top=q.front();
+            q.pop();
+            if(vis[top->val]==k+1)
+            {
+                ans.push_back(top->val);
+            }
+            if(vis[top->val]>k+1)
+                break;
+            if(par.find(top)!=par.end() && par[top] &&!vis[par[top]->val])
+            {
+                q.push(par[top]);
+                vis[par[top]->val]=1+vis[top->val];
+            }
+            if(top->right && !vis[top->right->val])
+            {
+                q.push(top->right);
+                vis[top->right->val]=1+vis[top->val];
+            }
+            if(top->left && !vis[top->left->val])
+            {
+                q.push(top->left);
+                vis[top->left->val]=1+vis[top->val];
+            }
+        }
+        return ans;
+    }
+};
