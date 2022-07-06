8c35b2a20f4f684617a4443142dbe7020fa82682
Nikhar Manchanda <60578798+NikharManchanda@users.noreply.github.com>
2022-07-07 02:51:07 +0530

    Time: 12 ms (95.16%), Space: 10.3 MB (24.57%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -19,27 +19,56 @@ public:
     //     return *min_element(grid[n-1].begin(),grid[n-1].end());
     // }
     
+    // Tabular DP Space O(n*n)
+
+//     int minFallingPathSum(vector<vector<int>>& grid) {
+//         int n=grid.size();
+//         vector<vector<int>>dp(n,vector<int>(n));
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             for(int j=0;j<n;j++)
+//             {
+//                 dp[i][j]=grid[i][j];
+//                 if(i>=1)
+//                 {
+//                     int val=dp[i-1][j];
+//                     if(j>=1)
+//                         val=min(val,dp[i-1][j-1]);
+//                     if(j+1<n)
+//                         val=min(val,dp[i-1][j+1]);
+//                     dp[i][j]+=val;
+//                 }
+//             }
+//         }
+//         return *min_element(dp[n-1].begin(),dp[n-1].end());
+//     }
+    
+    
+    // Tabular DP O(n)
     int minFallingPathSum(vector<vector<int>>& grid) {
         int n=grid.size();
-        vector<vector<int>>dp(n,vector<int>(n));
-        
+        vector<int> dp(n);        
         for(int i=0;i<n;i++)
         {
+            vector<int> cur(n);
             for(int j=0;j<n;j++)
             {
-                dp[i][j]=grid[i][j];
+                cur[j]=grid[i][j];
                 if(i>=1)
                 {
-                    int val=dp[i-1][j];
+                    int val=dp[j];
                     if(j>=1)
-                        val=min(val,dp[i-1][j-1]);
+                        val=min(val,dp[j-1]);
                     if(j+1<n)
-                        val=min(val,dp[i-1][j+1]);
-                    dp[i][j]+=val;
+                        val=min(val,dp[j+1]);
+                    cur[j]+=val;
                 }
             }
+            dp=cur;
         }
-        return *min_element(dp[n-1].begin(),dp[n-1].end());
+        return *min_element(dp.begin(),dp.end());
     }
+    
     
 };
