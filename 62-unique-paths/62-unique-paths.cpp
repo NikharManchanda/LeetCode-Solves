baa2b19e2896fff978f2e057fc4a36fa58a0b89a
Nikhar Manchanda <60578798+NikharManchanda@users.noreply.github.com>
2022-07-06 22:09:27 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (60.97%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -2,29 +2,44 @@ class Solution {
 public:
     
     // Tabular Dp
-//     int uniquePaths(int m, int n) {
-//         vector<vector<int>>dp(m,vector<int>(n,1));
-//         for(int i=1;i<m;i++)
-//         {
-//             for(int j=1;j<n;j++)
-//             {
-//                 dp[i][j]=dp[i-1][j]+dp[i][j-1];
-//             }
-//         }
-//         return dp[m-1][n-1];
-//     }
+    
+    // int uniquePaths(int m, int n) {
+    //     vector<vector<int>>dp(m,vector<int>(n,1));
+    //     for(int i=1;i<m;i++)
+    //     {
+    //         for(int j=1;j<n;j++)
+    //         {
+    //             dp[i][j]=dp[i-1][j]+dp[i][j-1];
+    //         }
+    //     }
+    //     return dp[m-1][n-1];
+    // }
     
     // Space optimised
+    
+    // int uniquePaths(int m, int n) {
+    //     vector<int> dp(n,1);
+    //     for(int i=1;i<m;i++)
+    //     {
+    //         vector<int> cur(n,1);
+    //         for(int j=1;j<n;j++)
+    //         {
+    //             cur[j]=dp[j]+cur[j-1];
+    //         }
+    //         dp=cur;
+    //     }
+    //     return dp[n-1];
+    // }
+    
+    // Further Optimsed
     int uniquePaths(int m, int n) {
         vector<int> dp(n,1);
         for(int i=1;i<m;i++)
         {
-            // vector<int> cur(n,1);
             for(int j=1;j<n;j++)
             {
                 dp[j]=dp[j]+dp[j-1];
             }
-            // dp=cur;
         }
         return dp[n-1];
     }
