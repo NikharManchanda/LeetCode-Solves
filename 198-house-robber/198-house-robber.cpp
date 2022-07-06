7a864daa182fe51705b2b73c69e443a40fc2dc60
Nikhar Manchanda <60578798+NikharManchanda@users.noreply.github.com>
2022-07-06 16:51:14 +0530

    Time: 4 ms (34.87%), Space: 7.6 MB (94.79%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,15 +1,34 @@
 class Solution {
 public:
+    // Tabulation
+    
+    // int rob(vector<int>& nums) {
+    //     int n=nums.size();
+    //     int max_money[n];
+    //     max_money[0]=nums[0];
+    //     for(int i=1;i<n;i++)
+    //     {
+    //         int val=max_money[i-1],val2=nums[i];
+    //         if(i>=2)val2+=max_money[i-2];
+    //         max_money[i]=max(val,val2);
+    //     }
+    //     return max_money[n-1];
+    // }
+    
+    
+    // Space Optimised
     int rob(vector<int>& nums) {
         int n=nums.size();
-        int max_money[n];
-        max_money[0]=nums[0];
+        int prev1=0,prev2=0;
+        prev1=nums[0];
         for(int i=1;i<n;i++)
         {
-            int val=max_money[i-1],val2=nums[i];
-            if(i>=2)val2+=max_money[i-2];
-            max_money[i]=max(val,val2);
+            int val=prev1,val2=nums[i];
+            if(i>=2)val2+=prev2;
+            int cur=max(val,val2);
+            prev2=prev1;
+            prev1=cur;
         }
-        return max_money[n-1];
+        return prev1;
     }
 };
