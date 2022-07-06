eabfdd714d766100d92f0e98bceb73cede9e0f9c
Nikhar Manchanda <60578798+NikharManchanda@users.noreply.github.com>
2022-07-06 22:06:23 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (57.02%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
+    
+    // Tabular Dp
+//     int uniquePaths(int m, int n) {
+//         vector<vector<int>>dp(m,vector<int>(n,1));
+//         for(int i=1;i<m;i++)
+//         {
+//             for(int j=1;j<n;j++)
+//             {
+//                 dp[i][j]=dp[i-1][j]+dp[i][j-1];
+//             }
+//         }
+//         return dp[m-1][n-1];
+//     }
+    
+    // Space optimised
     int uniquePaths(int m, int n) {
-        // vector<vector<int>>dp(m,vector<int>(n,1));
         vector<int> dp(n,1);
         for(int i=1;i<m;i++)
         {
@@ -14,4 +28,5 @@ public:
         }
         return dp[n-1];
     }
+    
 };
