47948daed4afbc24cad6369b9ae9102b5baa3ab9
Nikhar Manchanda <60578798+NikharManchanda@users.noreply.github.com>
2022-07-07 02:48:53 +0530

    Time: 15 ms (87.47%), Space: 10.3 MB (24.57%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,19 +1,45 @@
 class Solution {
 public:
+    // O(1) Space DP
+    
+    // int minFallingPathSum(vector<vector<int>>& grid) {
+    //     int n=grid.size();
+    //     for(int i=1;i<n;i++)
+    //     {
+    //         for(int j=n-1;j>=0;j--)
+    //         {
+    //             int val=grid[i-1][j];
+    //             if(j>=1)
+    //                 val=min(val,grid[i-1][j-1]);
+    //             if(j+1<n)
+    //                 val=min(val,grid[i-1][j+1]);
+    //             grid[i][j]+=val;
+    //         }
+    //     }
+    //     return *min_element(grid[n-1].begin(),grid[n-1].end());
+    // }
+    
     int minFallingPathSum(vector<vector<int>>& grid) {
         int n=grid.size();
-        for(int i=1;i<n;i++)
+        vector<vector<int>>dp(n,vector<int>(n));
+        
+        for(int i=0;i<n;i++)
         {
-            for(int j=n-1;j>=0;j--)
+            for(int j=0;j<n;j++)
             {
-                int val=grid[i-1][j];
-                if(j>=1)
-                    val=min(val,grid[i-1][j-1]);
-                if(j+1<n)
-                    val=min(val,grid[i-1][j+1]);
-                grid[i][j]+=val;
+                dp[i][j]=grid[i][j];
+                if(i>=1)
+                {
+                    int val=dp[i-1][j];
+                    if(j>=1)
+                        val=min(val,dp[i-1][j-1]);
+                    if(j+1<n)
+                        val=min(val,dp[i-1][j+1]);
+                    dp[i][j]+=val;
+                }
             }
         }
-        return *min_element(grid[n-1].begin(),grid[n-1].end());
+        return *min_element(dp[n-1].begin(),dp[n-1].end());
     }
+    
 };
