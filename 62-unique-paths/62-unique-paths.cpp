5f3e19d7984dc1a178540bf4ec50d502f8d93b61
Nikhar Manchanda <60578798+NikharManchanda@users.noreply.github.com>
2022-07-06 22:16:23 +0530

    Time: 3 ms (43.78%), Space: 5.9 MB (73.11%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -32,16 +32,30 @@ public:
     // }
     
     // Further Optimsed
-    int uniquePaths(int m, int n) {
-        vector<int> dp(n,1);
-        for(int i=1;i<m;i++)
-        {
-            for(int j=1;j<n;j++)
-            {
-                dp[j]=dp[j]+dp[j-1];
-            }
-        }
-        return dp[n-1];
+    
+    // int uniquePaths(int m, int n) {
+    //     vector<int> dp(n,1);
+    //     for(int i=1;i<m;i++)
+    //     {
+    //         for(int j=1;j<n;j++)
+    //         {
+    //             dp[j]=dp[j]+dp[j-1];
+    //         }
+    //     }
+    //     return dp[n-1];
+    // }
+    
+    // Math based solution
+    long long int nCr(int n, int r)
+    {
+        long long int ans=1;
+        for(int i=1;i<=r;i++)
+            ans=(ans*(n-r+i))/i;
+        return ans;
     }
+    int uniquePaths(int m, int n) {
+        return nCr(m-1+n-1,m-1);
+    }
+
     
 };
