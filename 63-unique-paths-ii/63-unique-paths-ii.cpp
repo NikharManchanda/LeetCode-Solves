45176944fd38b1877a80f99880ec44a5727f094f
Nikhar Manchanda <60578798+NikharManchanda@users.noreply.github.com>
2022-07-07 01:14:10 +0530

    Time: 4 ms (64.22%), Space: 7.5 MB (90.50%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -2,19 +2,35 @@ class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& a) {
         int n=a.size(),m=a[0].size();
-        vector<int>dp(m+1);
-        dp[1]=!a[0][0];
-        for(int i=1;i<=n;i++)
-        {
-            vector<int>cur(m+1);
-            for(int j=1;j<=m;j++)
-            {
-                if(!a[i-1][j-1])
-                    cur[j]=dp[j]+cur[j-1];
-            }
-            dp=cur;
-        }
-        return dp[m];
+        
+        //Space O(n)
+        // vector<int>dp(m+1);
+        // dp[0]=!a[0][0];
+        // for(int i=0;i<n;i++)
+        // {
+        //     for(int j=0;j<m;j++)
+        //     {
+        //         if(!a[i][j])
+        //         {
+        //             if(j>=1)
+        //                 dp[j]+=dp[j-1];
+        //         }
+        //         else
+        //             dp[j]=0;
+        //     }
+        // }
+        // return dp[m-1];
+    
+        //Space O(1)
+        a[0][0]^=1;
+        for(int i=1;i<n;i++)
+            a[i][0]=a[i-1][0] && !a[i][0];
+        for(int i=1;i<m;i++)
+            a[0][i]=a[0][i-1] && !a[0][i];
+        for(int i=1;i<n;i++)
+            for(int j=1;j<m;j++)
+                a[i][j]=(!a[i][j])?a[i-1][j]+a[i][j-1]:0;
+        return a[n-1][m-1];
     }
 
 };
