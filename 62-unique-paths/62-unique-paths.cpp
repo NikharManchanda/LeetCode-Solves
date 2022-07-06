be8e9560d8226d904ba36bafaf0edac81763e53d
Nikhar Manchanda <60578798+NikharManchanda@users.noreply.github.com>
2022-07-06 22:08:11 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (60.97%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -19,12 +19,12 @@ public:
         vector<int> dp(n,1);
         for(int i=1;i<m;i++)
         {
-            vector<int> cur(n,1);
+            // vector<int> cur(n,1);
             for(int j=1;j<n;j++)
             {
-                cur[j]=dp[j]+cur[j-1];
+                dp[j]=dp[j]+dp[j-1];
             }
-            dp=cur;
+            // dp=cur;
         }
         return dp[n-1];
     }
