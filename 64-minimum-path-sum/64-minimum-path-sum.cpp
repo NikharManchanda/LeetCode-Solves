28a060c17e161aa20e03e91b6755f6e6b08444a9
Nikhar Manchanda <60578798+NikharManchanda@users.noreply.github.com>
2022-07-06 22:46:48 +0530

    Time: 11 ms (78.32%), Space: 10 MB (51.80%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -20,29 +20,55 @@ public:
     //     return minPath(n-1,m-1,grid,dp);
     // }
     
-    // Tabulation 
     
+    // Tabulation 
+    // int minPathSum(vector<vector<int>>& grid) {
+    //     int n=grid.size(),m=grid[0].size();
+    //     vector<vector<int>>dp(n,vector<int>(m));        
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         for(int j=0;j<m;j++)
+    //         {
+    //             dp[i][j]=grid[i][j];
+    //             if(i==0 && j==0)
+    //             {
+    //                 continue;
+    //             }
+    //             if(i==0)
+    //                 dp[i][j]+=dp[i][j-1];
+    //             else if(j==0)
+    //                 dp[i][j]+=dp[i-1][j];
+    //             else 
+    //                 dp[i][j]+=min(dp[i-1][j],dp[i][j-1]);
+    //         }
+    //     }
+    //     return dp[n-1][m-1];
+    // }
+    
+    // Space Optimsed
     int minPathSum(vector<vector<int>>& grid) {
         int n=grid.size(),m=grid[0].size();
-        vector<vector<int>>dp(n,vector<int>(m));        
+        vector<int>dp(m);        
         for(int i=0;i<n;i++)
         {
+            vector<int> cur(m);
             for(int j=0;j<m;j++)
             {
-                dp[i][j]=grid[i][j];
+                cur[j]=grid[i][j];
                 if(i==0 && j==0)
                 {
                     continue;
                 }
                 if(i==0)
-                    dp[i][j]+=dp[i][j-1];
+                    cur[j]+=cur[j-1];
                 else if(j==0)
-                    dp[i][j]+=dp[i-1][j];
+                    cur[j]+=dp[j];
                 else 
-                    dp[i][j]+=min(dp[i-1][j],dp[i][j-1]);
+                    cur[j]+=min(dp[j],cur[j-1]);
             }
+            dp=cur;
         }
-        return dp[n-1][m-1];
+        return dp[m-1];
     }
     
     
