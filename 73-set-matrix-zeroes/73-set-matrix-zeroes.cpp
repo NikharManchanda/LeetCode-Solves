84cbaa42e77e3f370147c888a919a3e48ab253de
Nikhar Manchanda <60578798+NikharManchanda@users.noreply.github.com>
2022-07-08 13:00:34 +0530

    Time: 34 ms (12.64%), Space: 13.2 MB (81.77%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,29 +1,62 @@
 class Solution {
 public:
+    // Space O(n+n)
+    
+    // void setZeroes(vector<vector<int>>& grid) {
+    //     int n=grid.size(),m=grid[0].size();
+    //     vector<int> row(n),col(m);
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         for(int j=0;j<m;j++)
+    //         {
+    //             if(!grid[i][j])
+    //             {
+    //                 row[i]=1;
+    //                 col[j]=1;
+    //             }
+    //         }
+    //     }
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         if(row[i])
+    //         {
+    //             for(int j=0;j<m;j++)
+    //             {
+    //                 grid[i][j]=0;
+    //             }
+    //         }
+    //     }
+    //     for(int i=0;i<m;i++)
+    //     {
+    //         if(col[i])
+    //         {
+    //             for(int j=0;j<n;j++)
+    //             {
+    //                 grid[j][i]=0;
+    //             }
+    //         }
+    //     }
+    //     return ;
+    // }
+    
     // Space O(n)
     void setZeroes(vector<vector<int>>& grid) {
         int n=grid.size(),m=grid[0].size();
-        vector<int> row(n),col(m);
+        vector<int> col(m);
         for(int i=0;i<n;i++)
         {
+            bool ok=0;
             for(int j=0;j<m;j++)
             {
                 if(!grid[i][j])
                 {
-                    row[i]=1;
+                    ok=1;
                     col[j]=1;
                 }
             }
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(row[i])
-            {
+            if(ok)
                 for(int j=0;j<m;j++)
-                {
                     grid[i][j]=0;
-                }
-            }
         }
         for(int i=0;i<m;i++)
         {
