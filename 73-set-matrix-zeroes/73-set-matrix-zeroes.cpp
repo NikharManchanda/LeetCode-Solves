85aff491f1ca501a809c6f0b117b3f9cd6dcde3c
Nikhar Manchanda <60578798+NikharManchanda@users.noreply.github.com>
2022-07-08 12:57:42 +0530

    Time: 28 ms (27.82%), Space: 13.3 MB (53.10%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    // Space O(n)
+    void setZeroes(vector<vector<int>>& grid) {
+        int n=grid.size(),m=grid[0].size();
+        vector<int> row(n),col(m);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(!grid[i][j])
+                {
+                    row[i]=1;
+                    col[j]=1;
+                }
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(row[i])
+            {
+                for(int j=0;j<m;j++)
+                {
+                    grid[i][j]=0;
+                }
+            }
+        }
+        for(int i=0;i<m;i++)
+        {
+            if(col[i])
+            {
+                for(int j=0;j<n;j++)
+                {
+                    grid[j][i]=0;
+                }
+            }
+        }
+        return ;
+    }
+};
