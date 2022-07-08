b973ecf2a3db7d1f934df88ac7d72620b2a59430
Nikhar Manchanda <60578798+NikharManchanda@users.noreply.github.com>
2022-07-08 20:29:59 +0530

    Time: 25 ms (70.25%), Space: 19.5 MB (58.23%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,13 +1,32 @@
 class Solution {
 public:
+    // O(n) Space
+    
+    // int majorityElement(vector<int>& nums) {
+    //     unordered_map<int,int> mp;
+    //     for(int i=0;i<nums.size();i++)
+    //     {
+    //         mp[nums[i]]++;
+    //         if(mp[nums[i]]>nums.size()/2)
+    //             return nums[i];
+    //     }
+    //     return -1;
+    // }
+    
+    // Bank Algorithm
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        for(int i=0;i<nums.size();i++)
+        int ans=nums[0];
+        int count=1;
+        for(int i=1;i<nums.size();i++)
         {
-            mp[nums[i]]++;
-            if(mp[nums[i]]>nums.size()/2)
-                return nums[i];
+            if(count==0)
+                ans=nums[i];
+            if(nums[i]==ans)
+                count++;
+            else
+                count--;
         }
-        return -1;
+        return ans;
     }
+
 };
