e69b5c822d4ea9e426712ecda20eec974a259c48
Nikhar Manchanda <60578798+NikharManchanda@users.noreply.github.com>
2022-07-08 20:15:02 +0530

    Time: 16 ms (6.62%), Space: 9.5 MB (78.74%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,39 +1,64 @@
 class Solution {
 public:
+    // bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    //     int n=matrix.size(),m=matrix[0].size();
+    //     int l=0,r=n-1;
+    //     int row=-1;
+    //     while(l<=r)
+    //     {
+    //         int mid=(l+r)/2;
+    //         if(matrix[mid][0]<=target)
+    //         {
+    //             row=mid;
+    //             l=mid+1;
+    //         }
+    //         else
+    //         {
+    //             r=mid-1;
+    //         }
+    //     }
+    //     if(row==-1)
+    //         return 0;
+    //     l=0,r=m-1;
+    //     while(l<=r)
+    //     {
+    //         int mid=(l+r)/2;
+    //         if(matrix[row][mid]==target)
+    //             return 1;
+    //         if(matrix[row][mid]<target)
+    //         {
+    //             l=mid+1;
+    //         }
+    //         else
+    //         {
+    //             r=mid-1;
+    //         }
+    //     }
+    //     return 0;
+    // }
+    
+    // Single BS
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n=matrix.size(),m=matrix[0].size();
-        int l=0,r=n-1;
-        int row=-1;
-        while(l<=r)
+        int lo = 0;
+        if(!matrix.size()) return false;
+        int hi = (matrix.size() * matrix[0].size()) - 1;
+        
+        while(lo <= hi)
         {
-            int mid=(l+r)/2;
-            if(matrix[mid][0]<=target)
+            int mid = (lo + (hi - lo) / 2);
+            if(matrix[mid/matrix[0].size()][mid % matrix[0].size()] == target)
             {
-                row=mid;
-                l=mid+1;
+                return true;
+            }
+            if(matrix[mid/matrix[0].size()][mid % matrix[0].size()] < target)
+            {
+                lo = mid + 1;
             }
             else
             {
-                r=mid-1;
+                hi = mid - 1;
             }
         }
-        if(row==-1)
-            return 0;
-        l=0,r=m-1;
-        while(l<=r)
-        {
-            int mid=(l+r)/2;
-            if(matrix[row][mid]==target)
-                return 1;
-            if(matrix[row][mid]<target)
-            {
-                l=mid+1;
-            }
-            else
-            {
-                r=mid-1;
-            }
-        }
-        return 0;
+        return false;
     }
 };
