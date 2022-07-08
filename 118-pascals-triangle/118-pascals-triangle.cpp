0469ddefaac5df1b3ac95f2ce325e1ce0c68377c
Nikhar Manchanda <60578798+NikharManchanda@users.noreply.github.com>
2022-07-08 13:22:09 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (90.81%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -4,13 +4,10 @@ public:
         vector<vector<int>> ans(n);
         for(int i=0;i<n;i++)
         {
-            ans[i].push_back(1);
-            for(int j=1;j<=i;j++)
+            ans[i].resize(i+1,1);
+            for(int j=1;j<=i-1;j++)
             {
-                if(j!=i)
-                    ans[i].push_back(ans[i-1][j]+ans[i-1][j-1]);
-                else
-                    ans[i].push_back(ans[i-1][j-1]);
+                ans[i][j]=ans[i-1][j]+ans[i-1][j-1];                
             }
         }
         return ans;
