b45f71aa2bc75eb0f6b5949a45073554e227fadb
Nikhar Manchanda <60578798+NikharManchanda@users.noreply.github.com>
2022-07-14 04:06:15 +0530

    Time: 87 ms (17.75%), Space: 18.4 MB (46.75%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,13 +1,30 @@
 class Solution {
 public:
+    
+    // int change(int target, vector<int>& coins) {
+    //     vector<int>dp(target+1,0);
+    //     dp[0]=1;
+    //     for(int i=0;i<coins.size();i++){
+    //         for(int j=coins[i];j<=target;j++){
+    //             dp[j]+=dp[j-coins[i]];
+    //         }
+    //     }
+    //     return dp[target];
+    // }
+    
     int change(int target, vector<int>& coins) {
-        vector<int>dp(target+1,0);
-        dp[0]=1;
-        for(int i=0;i<coins.size();i++){
-            for(int j=coins[i];j<=target;j++){
-                dp[j]+=dp[j-coins[i]];
+        vector<vector<int>>dp(coins.size(),vector<int>(target+1,0));
+        for(int i=0;i<=target;i+=coins[0]){
+            dp[0][i]=1;
+        }
+        for(int i=1;i<coins.size();i++){
+            dp[i][0]=1;
+            for(int j=1;j<=target;j++){
+                dp[i][j]+=dp[i-1][j];
+                if(j-coins[i]>=0)
+                    dp[i][j]+=dp[i][j-coins[i]];
             }
         }
-        return dp[target];
+        return dp[coins.size()-1][target];
     }
 };
