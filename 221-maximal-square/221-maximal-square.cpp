02464a1959b23f1d0ec49c6802415ea229dcd491
Nikhar Manchanda <60578798+NikharManchanda@users.noreply.github.com>
2022-07-15 22:52:13 +0530

    Time: 350 ms (6.71%), Space: 18 MB (70.93%) - LeetHub

---
+++ 221-maximal-square/221-maximal-square.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int n=matrix.size(),m=matrix[0].size();
+        int ans=0;
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(matrix[i][j]=='0')
+                {
+                    continue;
+                }
+                int x,y,z;
+                x=(i>=1)?dp[i-1][j]:0;
+                y=(j>=1)?dp[i][j-1]:0;
+                z=(i>=1 && j>=1)?dp[i-1][j-1]:0;
+                if(x==y && y==z)
+                    dp[i][j]=x+1;
+                else
+                    dp[i][j]=1+min({x,y,z});
+                ans=max(ans,dp[i][j]);
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                cout<<dp[i][j]<<' ';
+            }
+            cout<<endl;
+        }
+        return ans*ans;
+    }
+};
