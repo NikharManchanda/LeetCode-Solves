fed9b21e502c1843ce711e3c001a602e0b01e48b
Nikhar Manchanda <60578798+NikharManchanda@users.noreply.github.com>
2022-07-27 21:55:41 +0530

    Time: 252 ms (5.09%), Space: 67.8 MB (53.44%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,14 +1,30 @@
 class Solution {
 public:
+    // Space O(n) DP
     int maxSubArray(vector<int>& nums) {
-        int sum=nums[0];
-        int prev=nums[0];
-        for(int i=1;i<nums.size();i++)
-        {
-            int cur=max(nums[i],prev+nums[i]);
-            sum=max(sum,cur);
-            prev=cur;
+      int max_sum=nums[0], sum=nums[0];
+      int n=nums.size();
+        for(int i=1; i<n; i++) {
+            sum = max(nums[i], sum+nums[i]);
+            if(sum>max_sum) {
+                max_sum=sum;
+            }
         }
-        return sum;
+        return max_sum;
     }
+    
+    // Space O(1) Dp , space optimisation 
+    // int maxSubArray(vector<int>& nums) {
+    //     int sum=nums[0];
+    //     int prev=nums[0];
+    //     for(int i=1;i<nums.size();i++)
+    //     {
+    //         int cur=max(nums[i],prev+nums[i]);
+    //         sum=max(sum,cur);
+    //         prev=cur;
+    //     }
+    //     return sum;
+    // }
+    
+    
 };
